42bb9b5c3b2e894d194ea60809bd413318092ad5
Lumy-dev <bibiasse309@gmail.com>
2024-10-10 23:49:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "50931461936"
-global.ownername = "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑"
+global.ownernomer = "242056717867"
+global.ownername = "🌹⃤ＩＬＬＵＭＩＮＡＴＩ🌹⃤𝐆𝐑𝐈𝐌𝐌⸥⟆𖥳🌹⃤"
 global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
 global.location = "America, Haiti, Port-au-Prince"
 
-global.ownernumber = '50931461936'  //creator number
+global.ownernumber = '242056717867'  //creator number
 global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
 global.botname = 'DRK_ST_BUG' //name of the bot
 
